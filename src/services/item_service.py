3ff1da461b2083d20531a55e8faccbae9e70aa92
Uxinnn <xinyu.foong@gmail.com>
2023-09-15 16:58:53 +0800

    Minor improvement on item_service code

--- src/services/item_service.py
+++ src/services/item_service.py
@@ -3,26 +3,32 @@ from db.models import Item
 from db.database import session
 
 
-def create(**kwargs):
-  item = Item(**kwargs)
+def create(raw_item):
+  item = Item(**raw_item)
   session.add(item)
   session.commit()
   session.refresh(item)
 
-  return item
+  return item.to_dict()
+
+
+def all_records():
+  items = select(Item)
+  result = session.execute(items).scalars().all()
+
+  return [item.to_dict() for item in result]
 
 
 def find(item_id):
   item = select(Item).where(Item.id == item_id)
   result = session.execute(item).scalars().one_or_none()
 
-  return result
+  return result.to_dict()
 
 
-def update_record(**kwargs):
-  _id = kwargs['id']
-  body = kwargs['body']
-  query = update(Item).where(Item.id == _id).values(body).execution_options(synchronize_session="fetch")
+def update_record(item):
+  _id = item['id']
+  query = update(Item).where(Item.id == _id).values(item).execution_options(synchronize_session="fetch")
   session.execute(query)
   session.commit()
 
@@ -31,8 +37,8 @@ def update_record(**kwargs):
   return item
 
 
-def delete_record(**kwargs):
-  item = session.query(Item).filter_by(id=kwargs['id']).first()
+def delete_record(_id):
+  item = session.query(Item).filter_by(id=_id).first()
 
   session.delete(item)
   session.commit()
