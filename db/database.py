d9eea40ebb968c1f1038fefd8235ed50609ec150
Uxinnn <xinyu.foong@gmail.com>
2023-09-15 16:01:42 +0800

    Added env file loading

--- db/database.py
+++ db/database.py
@@ -1,13 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-# TODO: Abstract these information into env file
-username = ""
-password = ""
-host = ""
-port = 0
-dbname = ""
+load_dotenv()
+
+username = os.environ.get("PSQL_USERNAME")
+password = os.environ.get("PSQL_PASSWORD")
+host = os.environ.get("PSQL_HOST")
+port = os.environ.get("PSQL_PORT")
+dbname = os.environ.get("PSQL_DB")
 
 engine = create_engine(f"postgresql://{username}:{password}@{host}:{port}/{dbname}")
 
