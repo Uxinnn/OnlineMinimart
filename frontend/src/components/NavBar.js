ed1eafca5b26d487d13ea9530366ac55e90c4cd7
Uxinnn <xinyu.foong@gmail.com>
2023-09-17 00:04:30 +0800

    Added logout in NavBar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Box, Typography, AppBar, Container, Toolbar, Button, IconButton, Drawer, List, ListItemButton, ListItemText } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 
 const NavBar = props => {
-    return (
-        <AppBar position="sticky">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters width="50px">
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+  return (
+    <AppBar position="sticky">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters width="50px">
+          <Box sx={{ flexGrow: 1 }}>
+            Pat's Minimart
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            <Button variant='contained' startIcon={<LogoutIcon />} sx={{ boxShadow: 0 }} href='/'>
+              Logout
+            </Button>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default NavBar;
