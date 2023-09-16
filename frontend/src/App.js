a8490d3dd69f8b63bb43d6e098e524b6e073d135
Uxinnn <xinyu.foong@gmail.com>
2023-09-16 23:45:30 +0800

    Added basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
 import React from 'react';
 import {Container, CssBaseline} from "@mui/material";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import ItemList from "./components/ItemList.js"
 import UserItemList from './components/UserItemList';
+import Home from './components/Home';
 import NavBar from "./components/NavBar.js"
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/', 
+      element: <Home />
+    }, 
+    {
+      path: "/user",
+      element: <UserItemList />,
+    },
+    {
+      path: "/admin",
+      element: <ItemList />,
+    },
+  ]);
+
   return (
-    <div>
-      <CssBaseline />
-      <NavBar/>
-      <Container maxWidth='xl'>
-        {/* <ItemList /> */}
-        <UserItemList />
-      </Container>
-    </div>
+      <div>
+        <CssBaseline />
+        <NavBar/>
+        <Container maxWidth='xl'>
+          <RouterProvider router={router} />
+        </Container>
+      </div>
   );
 }
 
